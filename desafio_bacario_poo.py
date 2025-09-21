c33946a704fe66bc54e38fee2ba2c9a6259742a0
Wilkerson Amorim <126714022+wklamorim@users.noreply.github.com>
2025-09-21 08:59:21 -0300

    Create desafio_bacario_poo.py
    Atualização do sistema bancário;
    Implementação de classes.

---
+++ desafio_bacario_poo.py
@@ -0,0 +1,141 @@
+from abc import ABC, abstractmethod, abstractproperty
+from datetime import datetime
+
+
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+
+    def adicionar_transacao(self, transacao):
+        self.transacoes.append(
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+            }
+        )
+
+
+class Transacao(ABC):
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+
+    @abstractmethod
+    def registrar(self, conta):
+        pass
+
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        transacao_realizada = conta.depositar(self.valor)
+
+        if transacao_realizada:
+            conta.historico.adicionar_transacao(self)
+
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        transacao_realizada = conta.sacar(self.valor)
+
+        if transacao_realizada:
+            conta.historico.adicionar_transacao(self)
+
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+
+class PessoaFisica(Cliente):
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        super().__init__(endereco)
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+
+    def __str__(self):
+        return f"""CPF: {self.cpf}\nCliente: {self.nome}\nData de nascimento: {self.data_nascimento}\nEndereço: {self.endereco}"""
+
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = '0001'
+        self._cliente = cliente
+        self._historico = Historico()
+
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls(cliente, numero)
+
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @property
+    def numero(self):
+        return self._numero
+
+    @property
+    def agencia(self):
+        return self._agencia
+
+    @property
+    def cliente(self):
+        return self._cliente
+
+    @property
+    def historico(self):
+        return self._historico
+
+
+class ContaCorrente(Conta):
+    contador = 0
+
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
+        self.limite = limite
+        self.limite_saques = limite_saques
+        ContaCorrente.contador += 1
+
+    def sacar(self, valor):
+        numero_saques = 0  # TODO (calcular numero de saques)
+
+        if numero_saques > self.limite_saques:
+            print('Excedeu a quantidade de saques por dia!')
+        elif valor > self.limite:
+            print('Excedeu o valor permitido por saque!')
+        else:
+            return super().sacar(valor)
+        return False
+
+    def __str__(self):
+        return f"""\nAgência: {self.agencia}\nConta: {self.numero}\nCliente: {self.cliente}"""
